617e788565e7594c89697bf4ee84cd0d0a7f08a2
OverflowYa <120401794+OverflowYa@users.noreply.github.com>
2023-09-20 17:30:42 +0300

    Add files via upload

---
+++ UseStaticMember/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UseStaticMember
+{
+    public partial class App : Application
+    {
+    }
+}
