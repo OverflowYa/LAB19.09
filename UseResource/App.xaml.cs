cf4a307e191e400fa520a2d04735f5c4cb7c8e0f
OverflowYa <120401794+OverflowYa@users.noreply.github.com>
2023-09-20 17:28:38 +0300

    Add files via upload

---
+++ UseResource/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UseResource
+{
+    public partial class App : Application
+    {
+    }
+}
